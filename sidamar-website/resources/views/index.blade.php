d6af7b503920d53ce45bcdffbea383bbde222fd9
abyanardiatama <abyanardiatama@students.undip.ac.id>
2023-01-27 17:44:50 +0700

    update login form

--- sidamar-website/resources/views/index.blade.php
+++ sidamar-website/resources/views/index.blade.php
@@ -6,11 +6,11 @@
   @vite('resources/css/app.css')
 </head>
 <body>
-  <h1 class="text-3xl font-bold underline bg-blue-200">
+  <h1 class="text-3xl font-bold bg-blue-200">
     <div class="min-h-screen w-full flex items-center justify-center bg-gray-50">
       <div>
-        <h1 class="mb-1 font-bold text-3xl flex gap-1 font-poppins text-center">Form UI</h1>
-    <div class="grid max-w-3xl gap-2 py-10 px-8 sm:grid-cols-2 bg-white rounded-md border-t-4 border-purple-400">
+        <h1 class="mb-5 p-5 font-bold text-center font-poppins text-6xl">Login</h1>
+    <div class="grid max-w-3xl gap-2 py-10 px-8 sm:grid-cols-2 bg-white rounded-md border-t-4 border-red-400">
       <div class="grid">
         <div class="bg-white flex min-h-[60px] flex-col-reverse justify-center rounded-md border border-gray-300 px-3 py-2 shadow-sm focus-within:shadow-inner">
           <input type="text" name="first-name" id="first-name" class="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0" placeholder="First name" />
@@ -34,8 +34,9 @@
           <input type="email" name="email" id="email" class="peer block w-full border-0 p-0 text-base text-gray-900 placeholder-gray-400 focus:ring-0" placeholder="E-mail" />
           <label html="email" class="block transform text-xs font-bold uppercase text-gray-400 transition-opacity, duration-200 peer-placeholder-shown:h-0 peer-placeholder-shown:-translate-y-full peer-placeholder-shown:opacity-0">E-mail</label>
         </div>
+        
       </div>
-             <button type="submit" class="mt-4 bg-purple-500 text-white py-2 px-6 rounded-md hover:bg-purple-600 ">Login</button>
+             <button type="submit" class="content-center w-100 mt-4 bg-red-500 text-white py-2 px-5 rounded-md hover:bg-red-500 mx-1">Login</button>
     </div>
     </div>
   </h1>

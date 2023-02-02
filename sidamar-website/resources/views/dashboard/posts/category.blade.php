f74764f7fa1833e36885eb72c66fbac4e1874429
Nurida Larasati <nrdlarast@students.undip.ac.id>
2023-02-02 15:03:30 +0700

    update category

--- sidamar-website/resources/views/dashboard/posts/category.blade.php
+++ sidamar-website/resources/views/dashboard/posts/category.blade.php
@@ -1,32 +1,32 @@
 @extends('dashboard.layouts.main')
 
 @section('container')
-   <h1>Ini Halaman category</h1>
+   <h1 class="font-bold mb-5 text-4xl">Post category</h1>
 
-   @foreach ($category as $cat)
-      <ul>
-         <li>{{ $cat->name }}</li>
-      </ul>
-      <table class="table-auto">
-         <thead>
+
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+         <thead class="text-xs text-gray-700 uppercase bg-white border-b dark:bg-gray-700 dark:text-gray-400">
            <tr>
-             <th>No</th>
-             <th>Name</th>
-             <th>Action</th>
+            <th scope="col" class="px-6 py-3">No</th>
+            <th scope="col" class="px-6 py-3">Name</th>
+            <th scope="col" class="px-6 py-3">Action</th>
            </tr>
          </thead>
+         
          <tbody>
-           <tr>
-             <td>{{ $cat->id }}</td>
-             <td>{{ $cat->name }}</td>
-             <td>
-               <a href=""><button class="rounded-full ">Edit</button></a>
-               <a href=""><button class="rounded-full ">Delete</button></a>
+   @foreach ($category as $cat)
+           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <td class="px-6 py-4">{{ $cat->id }}</td>
+            <td class="px-6 py-4">{{ $cat->name }}</td>
+            <td class="px-6 py-4">
+               <a href=""class="font-medium text-blue-600 dark:text-blue-500 hover:underline"><button class="rounded-full ">Edit</button></a>
+               <a href=""class="font-medium text-blue-600 dark:text-blue-500 hover:underline"><button class="rounded-full ">Delete</button></a>
              </td>
            </tr>
+   @endforeach           
          </tbody>
        </table> 
-   @endforeach
+
        
    <div class="mb-96"></div>
 @endsection

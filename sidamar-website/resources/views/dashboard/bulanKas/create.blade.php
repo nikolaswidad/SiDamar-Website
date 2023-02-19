e8267bd00f31f47a29eb9398f882675d99fac8c4
abyanardiatama <abyanardiatama@students.undip.ac.id>
2023-02-19 14:51:39 +0700

    update bulan_kas to bulanKas

--- sidamar-website/resources/views/dashboard/bulanKas/create.blade.php
+++ sidamar-website/resources/views/dashboard/bulanKas/create.blade.php
@@ -5,13 +5,15 @@
 
 <div class="max-w-5xl">
     <hr class="bg-slate-200 mt-5 max-w-3xl">
-      <form action="/dashboard/bulan_kas/" method="post" class="max-w-3xl mt-8 mb-8 font-montserrat">
+      <form action="/dashboard/bulanKas/" method="post" class="max-w-3xl mt-8 mb-8 font-montserrat">
         @csrf
                 
         {{-- Input Bulan type dropdown with cool tailwind style --}}
         <div class="mb-4">
             <label for="bulan" class="block text-gray-700 text-lg font-bold mb-2">Bulan</label>
             <select name="bulan" id="bulan" class="h-11 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline mb-5">
+                {{-- select old value if any --}}
+                <option value="">Pilih Bulan</option>
                 <option value="Januari">Januari</option>
                 <option value="Februari">Februari</option>
                 <option value="Maret">Maret</option>
@@ -27,14 +29,17 @@
             </select>
 
             @error('bulan')
-                <div class="text-red-500 text-sm mt-2">{{ $message }}</div>
+                <div class="text-red-500 text-sm mb-2">{{ $message }}</div>
             @enderror
 
             {{-- field tahun berdasarkan tahun saat ini --}}
             <label for="tahun" class="block text-gray-700 text-lg font-bold mb-2">Tahun</label>
             <select name="tahun" id="tahun" class="h-11 block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline mb-5">
-                <option value="{{ now()->year }}">{{ now()->year }}</option>
-                {{-- @for ($i = 2020; $i <= 2030; $i++)
+                <option value="">Pilih Tahun</option>
+                @for ($i = now()->year; $i >= now()->year - 10; $i--)
+                    <option value="{{ $i }}" {{ old('tahun') == $i ? 'selected' : '' }}>{{ $i }}</option>
+                @endfor
+                {{-- @for ($i = now()->year; $i >= now()->year - 10; $i--)
                     <option value="{{ $i }}">{{ $i }}</option>
                 @endfor --}}
             </select>

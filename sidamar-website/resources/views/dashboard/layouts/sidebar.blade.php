6cb26478b1d4966903bdc479346b2ea921741fad
abyanardiatama <abyanardiatama@students.undip.ac.id>
2023-05-18 10:50:14 +0700

    update slug

--- sidamar-website/resources/views/dashboard/layouts/sidebar.blade.php
+++ sidamar-website/resources/views/dashboard/layouts/sidebar.blade.php
@@ -691,7 +691,7 @@
          <ul class="space-y-2 font-medium">
             <span class="flex items-center  px-4 pt-4 text-md text-gray-400 rounded-lg dark:text-white">Author</span>
             <li>
-               <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <a href="/dashboard/posts" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                   <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                      <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"></path>
                    </svg>

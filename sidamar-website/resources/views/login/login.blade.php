9526e4c4339533042f39f2a09a2c8d06b069d2ef
nikolaswidad <70227834+nikolaswidad@users.noreply.github.com>
2023-05-31 12:14:59 +0700

    footer and login button

--- sidamar-website/resources/views/login/login.blade.php
+++ sidamar-website/resources/views/login/login.blade.php
@@ -6,7 +6,7 @@
         <div>
 
             @if (session()->has('loginError'))
-            <div class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-500" role="alert">
+            <div class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
                 <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
                 <span class="sr-only">Info</span>
                 <div>
@@ -54,7 +54,7 @@
             </div>
 
             <div>
-            <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-primary py-2 px-4 text-sm font-medium text-white hover:bg-primaryDarken focus:outline-none focus:ring-2 focus:ring-primaryLighten focus:ring-offset-2">
+            <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent  py-2 px-4 text-sm font-medium text-white bg-red-600 hover:bg-primaryDarken focus:outline-none focus:ring-2 focus:ring-primaryLighten focus:ring-offset-2">
                 Sign in
             </button>
         </div>

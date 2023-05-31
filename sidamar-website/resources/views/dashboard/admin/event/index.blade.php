d86396c5c0f3460a296c68bee3038dd1288a018f
nikolaswidad <70227834+nikolaswidad@users.noreply.github.com>
2023-05-31 14:14:30 +0700

    main feature fix

--- sidamar-website/resources/views/dashboard/admin/event/index.blade.php
+++ sidamar-website/resources/views/dashboard/admin/event/index.blade.php
@@ -16,15 +16,6 @@
     <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
     </svg> Trash</button>
    </a>
-   {{-- <div>
-     <label for="table-search" class="sr-only">Search</label>
-     <div class="relative">
-         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-             <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
-         </div>
-         <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items">
-     </div>
-   </div> --}}
     <div>
       <label for="table-search" class="sr-only">Search</label>
       <div class="relative">
--- sidamar-website/resources/views/dashboard/admin/present/index.blade.php
+++ sidamar-website/resources/views/dashboard/admin/present/index.blade.php
@@ -12,7 +12,7 @@
                 <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                 </div>
-                <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-red-500 focus:border-red-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500" placeholder="Search for items">
+                <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-red-500 focus:border-red-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500" placeholder="Cari apa saja">
             </div>
         </div>
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -122,7 +122,7 @@
         </table>
     </div>
 
-    <script>
+    {{-- <script>
         const tableSearchInput = document.getElementById('table-search');
         const tableRows = document.querySelectorAll('tbody tr');
       
@@ -132,12 +132,48 @@
           tableRows.forEach(function (row) {
             const eventTitle = row.querySelector('th').textContent.toLowerCase();
       
-            if (eventTitle.includes(searchValue)) {
+            if (
+                eventTitle.includes(searchValue) ||
+                eventCategory.includes(searchValue) ||
+                eventDate.includes(searchValue) ||
+                eventLocation.includes(searchValue)
+            ) {
               row.style.display = '';
             } else {
               row.style.display = 'none';
             }
           });
         });
-      </script>
+        
+      </script> --}}
+
+    <script>
+        const tableSearchInput = document.getElementById('table-search');
+        const tableRows = document.querySelectorAll('tbody tr');
+
+        tableSearchInput.addEventListener('input', function () {
+            const searchValue = this.value.toLowerCase();
+
+            tableRows.forEach(function (row) {
+                let isRowVisible = false;
+                const cells = row.querySelectorAll('td');
+
+                cells.forEach(function (cell) {
+                    const cellContent = cell.textContent.toLowerCase();
+
+                    if (cellContent.includes(searchValue)) {
+                        isRowVisible = true;
+                    }
+                });
+
+                if (isRowVisible) {
+                    row.style.display = '';
+                } else {
+                    row.style.display = 'none';
+                }
+            });
+        });
+    </script>
+
+    
 @endsection

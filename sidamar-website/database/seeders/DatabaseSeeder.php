b5c6d84bbfaf309776f2f783f11049f15af616db
MelaniSafwa <112703343+MelaniSafwa@users.noreply.github.com>
2023-06-06 10:21:48 +0700

    tambahan seeder

--- sidamar-website/database/seeders/DatabaseSeeder.php
+++ sidamar-website/database/seeders/DatabaseSeeder.php
@@ -10,6 +10,11 @@ use App\Models\Event;
 use App\Models\BulanKas;
 use App\Models\PostCategory;
 use App\Models\EventCategory;
+use App\Models\finance;
+use App\Models\donation;
+use App\Models\Donatur;
+use App\Models\Merch;
+use App\Models\Customer;
 use Illuminate\Database\Seeder;
 
 
@@ -117,5 +122,143 @@ class DatabaseSeeder extends Seeder
             'tahun' => '2022'
         ]);
     // Bulan Kas seeder end
-    }
+
+
+    //finance seeder start
+    finance::create([
+        'keperluan' => 'Membayar Pajak',
+        'date' => '2023-05-06',
+        'cashin' => 0,
+        'cashout' => 20000,
+        'keterangan' => '-'
+    ]);
+
+    finance::create([
+        'keperluan' => 'Uang Kas',
+        'date' => '2023-05-20',
+        'cashin' => 150000,
+        'cashout' => 0,
+        'keterangan' => 'Nitip El'
+    ]);
+
+    finance::create([
+        'keperluan' => 'Iuran Tenda',
+        'date' => '2023-06-20',
+        'cashin' => 0,
+        'cashout' => 350000,
+        'keterangan' => '-'
+    ]);
+    //finance seeder end
+
+    //donation seeder start
+
+    donation::create([
+        'title' => 'Berbagi Kebahagiaan Bersama Anak Yatim Piatu',
+        'body' => 'Tidak semua anak mampu menghabiskan waktu spesial ini dengan keluarga inti mereka. Salah satunya adalah anak yatim. Karenanya, sangatlah penting untuk bisa berbagi kebahagiaan kepada anak yatim. SI DAMAR mengajak kalian bersama - sama untuk saling berbagi kebahagiaan dengan saudara kita. Mari berbagi kebahagiaan!!1',
+        'date' => '2023-05-26',
+        'image' => 'upload/donation/1684681513Berbagi bersama anak yatim piatu.jpg'
+    ]);
+
+    donation::create([
+        'title' => 'Kerja Bakti Bersama SI DAMAR',
+        'body' => 'Kerja bakti membersihkan lingkungan rumah biasanya dilakukan dalam lingkup rukun tetangga (RT) dan rukun warga (RW). Kerja bakti tersebut dilakukan oleh semua warga dalam lingkup RT dan RW yang bertujuan untuk mencapai lingkungan hidup yang bersih dan asri. Seluruh warga juga harus bekerja sama untuk tidak membuang sampah secara sembarangan agar kebersihan dan keasrian lingkungan terjaga.&nbsp;',
+        'date' => '2023-06-10',
+        'image' => 'upload/donation/1684684561Kerja Bakti.jpg'
+    ]);
+
+    //donation seeder end
+
+    //donatur seeder start
+    Donatur::create([
+        'name' => 'Melani Safwa Aprila',
+        'donation_id' => '3',
+        'no_hp' => '+62859155035552',
+        'email' => 'melanisafwa1304@gmail.com',
+        'payment_type' => 'shopeepay',
+        'image' => 'upload/donatur/1684686762Screenshot (721).png',
+        'total' => 50000,
+    ]);
+
+    Donatur::create([
+        'name' => 'Nurida Larasati',
+        'donation_id' => '2',
+        'no_hp' => '+62859155035552',
+        'email' => 'nurida304@gmail.com',
+        'payment_type' => 'bni',
+        'image' => 'upload/donatur/1684686762Screenshot (721).png',
+        'total' => 50000,
+    ]);
+
+    Donatur::create([
+        'name' => 'Chantika',
+        'donation_id' => '1',
+        'no_hp' => '+62859155035552',
+        'email' => 'caca1304@gmail.com',
+        'payment_type' => 'bca',
+        'image' => 'upload/donatur/1684686762Screenshot (721).png',
+        'total' => 50000,
+    ]);
+
+     //donatur seeder end
+
+    //merch seeder start
+    Merch::create([
+        'title' => 'Tote Bag',
+        'desc' => 'Berbahan katun, kuat, cakep banget, cocok buat main wkwkwkwkwkwkYuk dibeli!!!',
+        'price' => 50000,
+        'image' => 'upload/merches/1683337693a1557-cotton-printed-tote-bag-5oz-natural.png'
+    ]);
+
+    Merch::create([
+        'title' => 'Tumblr Botol Minum',
+        'desc' => 'Berbahan katun, kuat, cakep banget, cocok buat main wkwkwkwkwkwkYuk dibeli!!!',
+        'price' => 40000,
+        'image' => 'upload/merches/1683337722cf5f016652a4444fe41cd39ecf17a01f.jpg'
+    ]);
+
+    Merch::create([
+        'title' => 'Kaos Sidamar',
+        'desc' => 'Berbahan katun, kuat, cakep banget, cocok buat main wkwkwkwkwkwkYuk dibeli!!!',
+        'price' => 75000,
+        'image' => 'upload/merches/1683337748Kaos-Polos.jpg'
+    ]);
+    //merch seeder end
+
+    //customer seeder start
+
+    Customer::create([
+        'name' => 'Chantika',
+        'merch_id' => '1',
+        'no_hp' => '+62859155035552',
+        'email' => 'caca1304@gmail.com',
+        'payment_type' => 'bca',
+        'address' => 'Semarang',
+        'image' => 'upload/customer/1684956543Screenshot (770).png',
+        'total' => 50000,
+    ]);
+
+    Customer::create([
+        'name' => 'Seo Changbin',
+        'merch_id' => '3',
+        'no_hp' => '+62859155035552',
+        'email' => 'caca1304@gmail.com',
+        'payment_type' => 'shopeepay',
+        'address' => 'Semarang',
+        'image' => 'upload/customer/1685972802WhatsApp Image 2023-06-02 at 8.25.34 PM.jpeg',
+        'total' => 50000,
+    ]);
+
+    Customer::create([
+        'name' => 'Melani Safwa Aprila',
+        'merch_id' => '2',
+        'no_hp' => '+62859155035552',
+        'email' => 'caca1304@gmail.com',
+        'payment_type' => 'shopeepay',
+        'address' => 'Semarang',
+        'image' => 'upload/customer/1685972802WhatsApp Image 2023-06-02 at 8.25.34 PM.jpeg',
+        'total' => 50000,
+    ]);
+
+    //customer seeder start
+}
 }

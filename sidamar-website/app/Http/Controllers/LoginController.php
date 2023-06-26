287dbc62830108b56eb939e5b68cd7083bce1618
nikolaswidad <70227834+nikolaswidad@users.noreply.github.com>
2023-06-26 21:57:04 +0700

    fix

--- sidamar-website/app/Http/Controllers/LoginController.php
+++ sidamar-website/app/Http/Controllers/LoginController.php
@@ -23,7 +23,7 @@ class LoginController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect()->intended('/dashboard');
+            return redirect()->intended('/dashboard/event');
         }
 
         return back()->with('loginError', 'Login failed!');
